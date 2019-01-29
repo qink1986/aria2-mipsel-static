26a2a81ae640cc02a700fb4aaac2811ec59b2b65
Jason <qink1986@gmail.com>
2019-01-29 13:20:48 +0800

    Update aria2.sh
    fix for openwrt

--- aria2.sh
+++ aria2.sh
@@ -9,32 +9,32 @@ PREFIX=/opt
 
 BASE=`pwd`
 SRC=$BASE/src
-WGET="wget --prefer-family=IPv4"
+WGET="tsocks wget --prefer-family=IPv4"
 DEST=$BASE$PREFIX
 LDFLAGS="-L$DEST/lib -Wl,--gc-sections"
 CPPFLAGS="-I$DEST/include"
-CFLAGS="-mtune=mips32 -mips32 -O3 -ffunction-sections -fdata-sections"	
+CFLAGS="-mtune=mips32 -mips32 -O3 -ffunction-sections -fdata-sections -lpthread"	
 CXXFLAGS=$CFLAGS
-CONFIGURE="./configure --prefix=$PREFIX --host=mipsel-linux"
+CONFIGURE="./configure --prefix=$PREFIX --host=mipsel-openwrt-linux"
 MAKE="make -j`nproc`"
-mkdir $SRC
+#mkdir $SRC
 
 ######## ####################################################################
 # ZLIB # ####################################################################
 ######## ####################################################################
 
-mkdir $SRC/zlib && cd $SRC/zlib
-$WGET http://zlib.net/zlib-1.2.8.tar.gz
-tar zxvf zlib-1.2.8.tar.gz
-cd zlib-1.2.8
+#mkdir $SRC/zlib && cd $SRC/zlib
+#$WGET http://zlib.net/zlib-1.2.11.tar.gz
+tar zxvf zlib-1.2.11.tar.gz
+cd zlib-1.2.11
 
 LDFLAGS=$LDFLAGS \
-CPPFLAGS=$CPPFLAGS \
-CFLAGS=$CFLAGS \
-CXXFLAGS=$CXXFLAGS \
-CROSS_PREFIX=mipsel-linux- \
-./configure \
---prefix=$PREFIX
+	CPPFLAGS=$CPPFLAGS \
+	CFLAGS=$CFLAGS \
+	CXXFLAGS=$CXXFLAGS \
+	CROSS_PREFIX=mipsel-openwrt-linux- \
+	./configure \
+	--prefix=$PREFIX
 
 $MAKE
 make install DESTDIR=$BASE
@@ -43,35 +43,35 @@ make install DESTDIR=$BASE
 # OPENSSL # #################################################################
 ########### #################################################################
 
-mkdir -p $SRC/openssl && cd $SRC/openssl
-$WGET https://www.openssl.org/source/openssl-1.0.2g.tar.gz
+#mkdir -p $SRC/openssl && cd $SRC/openssl
+#$WGET https://www.openssl.org/source/openssl-1.0.2g.tar.gz
 tar zxvf openssl-1.0.2g.tar.gz
 cd openssl-1.0.2g
 
 ./Configure linux-mips32 \
--mtune=mips32 -mips32 \
--ffunction-sections -fdata-sections -Wl,--gc-sections \
---prefix=$PREFIX shared zlib \
---with-zlib-lib=$DEST/lib \
---with-zlib-include=$DEST/include
+	-mtune=mips32 -mips32 \
+	-ffunction-sections -fdata-sections -Wl,--gc-sections \
+	--prefix=$PREFIX shared zlib \
+	--with-zlib-lib=$DEST/lib \
+	--with-zlib-include=$DEST/include
 
-make CC=mipsel-linux-gcc
-make CC=mipsel-linux-gcc install INSTALLTOP=$DEST OPENSSLDIR=$DEST/ssl
+make CC=mipsel-openwrt-linux-gcc
+make CC=mipsel-openwrt-linux-gcc install INSTALLTOP=$DEST OPENSSLDIR=$DEST/ssl
 
 ########## ##################################################################
-# SQLITE # ##################################################################
+# SQLITE # # v3.12.1 ########################################################
 ########## ##################################################################
 
-mkdir $SRC/sqlite && cd $SRC/sqlite
-$WGET https://www.sqlite.org/2016/sqlite-autoconf-3120100.tar.gz --no-check-certificate
-tar zxvf sqlite-autoconf-3120100.tar.gz
-cd sqlite-autoconf-3120100
+#mkdir $SRC/sqlite && cd $SRC/sqlite
+#$WGET https://www.sqlite.org/cgi/src/tarball/fe7d3b75/SQLite-fe7d3b75.tar.gz --no-check-certificate
+#tar zxvf SQLite-fe7d3b75.tar.gz
+cd SQLite-fe7d3b75
 
 LDFLAGS=$LDFLAGS \
-CPPFLAGS=$CPPFLAGS \
-CFLAGS=$CFLAGS \
-CXXFLAGS=$CXXFLAGS \
-$CONFIGURE
+	CPPFLAGS=$CPPFLAGS \
+	CFLAGS=$CFLAGS \
+	CXXFLAGS=$CXXFLAGS \
+	$CONFIGURE
 
 make
 make install DESTDIR=$BASE
@@ -86,12 +86,12 @@ tar zxvf libxml2-2.9.3.tar.gz
 cd libxml2-2.9.3
 
 LDFLAGS=$LDFLAGS \
-CPPFLAGS=$CPPFLAGS \
-CFLAGS=$CFLAGS \
-CXXFLAGS=$CXXFLAGS \
-$CONFIGURE \
---with-zlib=$DEST \
---without-python
+	CPPFLAGS=$CPPFLAGS \
+	CFLAGS=$CFLAGS \
+	CXXFLAGS=$CXXFLAGS \
+	$CONFIGURE \
+	--with-zlib=$DEST \
+	--without-python
 
 $MAKE LIBS="-lz"
 make install DESTDIR=$BASE
@@ -106,10 +106,10 @@ tar zxvf c-ares-1.11.0.tar.gz
 cd c-ares-1.11.0
 
 LDFLAGS=$LDFLAGS \
-CPPFLAGS=$CPPFLAGS \
-CFLAGS=$CFLAGS \
-CXXFLAGS=$CXXFLAGS \
-$CONFIGURE
+	CPPFLAGS=$CPPFLAGS \
+	CFLAGS=$CFLAGS \
+	CXXFLAGS=$CXXFLAGS \
+	$CONFIGURE
 
 $MAKE
 make install DESTDIR=$BASE
@@ -124,10 +124,10 @@ tar zxvf libssh2-1.7.0.tar.gz
 cd libssh2-1.7.0
 
 LDFLAGS=$LDFLAGS \
-CPPFLAGS=$CPPFLAGS \
-CFLAGS=$CFLAGS \
-CXXFLAGS=$CXXFLAGS \
-$CONFIGURE
+	CPPFLAGS=$CPPFLAGS \
+	CFLAGS=$CFLAGS \
+	CXXFLAGS=$CXXFLAGS \
+	$CONFIGURE
 
 $MAKE LIBS="-lz -lssl -lcrypto"
 make install DESTDIR=$BASE
@@ -137,37 +137,38 @@ make install DESTDIR=$BASE
 ######### ###################################################################
 
 mkdir $SRC/aria2 && cd $SRC/aria2
-$WGET https://github.com/tatsuhiro-t/aria2/releases/download/release-1.21.0/aria2-1.21.0.tar.gz
-tar zxvf aria2-1.21.0.tar.gz
-cd aria2-1.21.0
+$WGET https://github.com/aria2/aria2/releases/download/release-1.34.0/aria2-1.34.0.tar.gz
+tar zxvf aria2-1.34.0.tar.gz
+cd aria2-1.34.0
 
 LDFLAGS="-zmuldefs $LDFLAGS" \
-CPPFLAGS=$CPPFLAGS \
-CFLAGS=$CFLAGS \
-CXXFLAGS=$CXXFLAGS \
-$CONFIGURE \
---enable-libaria2 \
---enable-static \
---disable-shared \
---without-libuv \
---without-appletls \
---without-gnutls \
---without-libnettle \
---without-libgmp \
---without-libgcrypt \
---without-libexpat \
---with-xml-prefix=$DEST \
-ZLIB_CFLAGS="-I$DEST/include" \
-ZLIB_LIBS="-L$DEST/lib" \
-OPENSSL_CFLAGS="-I$DEST/include" \
-OPENSSL_LIBS="-L$DEST/lib" \
-SQLITE3_CFLAGS="-I$DEST/include" \
-SQLITE3_LIBS="-L$DEST/lib" \
-LIBCARES_CFLAGS="-I$DEST/include" \
-LIBCARES_LIBS="-L$DEST/lib" \
-LIBSSH2_CFLAGS="-I$DEST/include" \
-LIBSSH2_LIBS="-L$DEST/lib" \
-ARIA2_STATIC=yes
+	CPPFLAGS=$CPPFLAGS \
+	CFLAGS=$CFLAGS \
+	CXXFLAGS=$CXXFLAGS \
+	$CONFIGURE \
+	--enable-libaria2 \
+	--enable-static \
+	--disable-shared \
+	--without-libuv \
+	--without-appletls \
+	--without-gnutls \
+	--without-libnettle \
+	--without-libgmp \
+	--without-libgcrypt \
+	--without-libexpat \
+	--with-xml-prefix=$DEST \
+	ZLIB_CFLAGS="-I$DEST/include" \
+	ZLIB_LIBS="-L$DEST/lib" \
+	OPENSSL_CFLAGS="-I$DEST/include" \
+	OPENSSL_LIBS="-L$DEST/lib" \
+	SQLITE3_CFLAGS="-I$DEST/include" \
+	SQLITE3_LIBS="-L$DEST/lib" \
+	LIBCARES_CFLAGS="-I$DEST/include" \
+	LIBCARES_LIBS="-L$DEST/lib" \
+	LIBSSH2_CFLAGS="-I$DEST/include" \
+	LIBSSH2_LIBS="-L$DEST/lib" \
+	ARIA2_STATIC=yes
 
 $MAKE LIBS="-lz -lssl -lcrypto -lsqlite3 -lcares -lxml2 -lssh2"
 make install DESTDIR=$BASE/aria2
+
