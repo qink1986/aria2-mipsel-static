2c323db1dd170e558a4bed3d3adaf9cfbe1dbe0e
qink_dell <qink1986@gmail.com>
2019-01-29 16:32:31 +0800

    drop fix libssh2 prefix

--- aria2.sh
+++ aria2.sh
@@ -128,7 +128,7 @@ LDFLAGS=$LDFLAGS \
 	CPPFLAGS=$CPPFLAGS \
 	CFLAGS=$CFLAGS \
 	CXXFLAGS=$CXXFLAGS \
-	./configure --prefix=$PREFIX --host=mipsel-openwrt-linux --with-libssl-prefix=/opt/lib/
+	$CONFIGURE
 
 $MAKE LIBS="-lz -lssl -lcrypto"
 make install DESTDIR=$BASE
